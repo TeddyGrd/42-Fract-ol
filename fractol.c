212b6a68104387cb42a49f9033b840e5b824bfd5
TeddyGrd <teddyguerrand76@gmail.com>
2024-01-23 22:03:10 +0100

    Ajout des evenement sur le screen

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: tguerran <tguerran@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 11:30:37 by tguerran          #+#    #+#             */
-/*   Updated: 2024/01/23 19:43:20 by tguerran         ###   ########.fr       */
+/*   Updated: 2024/01/23 22:01:48 by tguerran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,40 +24,20 @@ int	main(int argc, char* argv[])
 	img.img_str = mlx_get_data_addr(img.img, &img.bits, &img.size_line, &img.endian);
 	
 	// initialise la fractal ici
-	t_fractal mandelbrot;
-	initialize_mandelbrot(&mandelbrot);
-	draw_mandelbrot(img.img_str, &mandelbrot);
+	t_fractal julia;
+	initialize_julia(&julia);
+	draw_julia(img.img_str, &julia);
 	// fin de l init
 	
 	mlx_put_image_to_window(data.mlx, data.win,img.img, 0, 0);
 	mlx_get_data_addr(img.img,&img.bits ,&img.size_line, &img.endian);
 
-	// mlx_mouse_hook(data.win,mouse_zoom, &data);
-	// mlx_hook(data.win, 33, 0, mouse_close, &data);
+	mlx_mouse_hook(data.win,mouse_zoom, &data);
+	mlx_hook(data.win, 33, 0, mouse_close, &data);
+	mlx_key_hook(data.win,handle_key_press ,NULL);
     mlx_do_sync(data.mlx);
 	mlx_loop(data.mlx);
 	mlx_destroy_image(data.mlx, img.img);
     sleep(5);
     return (0);
 }
-
-// int main() {
-//     t_img img;
-//     void *mlx_ptr = mlx_init();
-//     void *win1 = mlx_new_window(mlx_ptr, WIDTH, HEIGHT, "Julia Set");
-
-//     char *image1 = mlx_new_image(mlx_ptr, WIDTH, HEIGHT);
-//     char *image_data1 = mlx_get_data_addr(image1, &img.bits , &img.size_line, &img.endian);
-//     t_fractal julia;
-
-//     initialize_julia(&julia);
-
-// 	draw_julia(image_data1, &julia);
-// 	mlx_put_image_to_window(mlx_ptr, win1, image1, 0, 0);
-// 	mlx_do_sync(mlx_ptr);
-
-//     mlx_loop(mlx_ptr);
-
-
-//     return (0);
-// }
--- hook/keyboard.c
+++ hook/keyboard.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   event.c                                            :+:      :+:    :+:   */
+/*   keyboard.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tguerran <tguerran@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 21:37:15 by tguerran          #+#    #+#             */
-/*   Updated: 2024/01/22 21:44:27 by tguerran         ###   ########.fr       */
+/*   Updated: 2024/01/23 22:02:18 by tguerran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-
-int handle_key_press(int keycode, t_fractal *mandelbrot) {
-    double zoom_factor = 0.8; 
-    if (keycode == 65362)
-	{
-		printf("ici \n");
-		zoom(mandelbrot, zoom_factor);
-	}
-    else if (keycode == 65364)
-	{
-        zoom(mandelbrot, 1.0 / zoom_factor);
-		printf("Touche pressée : %d\n", keycode);
-	}
-	else if(keycode == 65307)
+int handle_key_press(int keycode, t_fractal *fractal) {
+    double zoom_factor = 0.8;
+	// printf("%d",keycode);
+    // if (keycode == 65362)
+	// {
+	// 	printf("ici \n");
+	// 	zoom(mandelbrot, zoom_factor);
+	// }
+    // else if (keycode == 65364)
+	// {
+    //     zoom(mandelbrot, 1.0 / zoom_factor);
+	// 	printf("Touche pressée : %d\n", keycode);
+	// }
+	if(keycode == 65307)
 		exit (0);
     return 0;
 }
