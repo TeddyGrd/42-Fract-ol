215cbc0a1ec8061b4890a2a7f037c764325e29e1
TeddGrd <teddyguerrand76@gmail.com>
2024-02-26 02:13:13 +0100

    norminette clean

--- fractol_utils.c
+++ fractol_utils.c
@@ -6,61 +6,55 @@
 /*   By: tguerran <tguerran@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 14:38:18 by tguerran          #+#    #+#             */
-/*   Updated: 2024/02/22 18:15:30 by tguerran         ###   ########.fr       */
+/*   Updated: 2024/02/26 02:12:06 by tguerran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int ft_strcmp(const char *s1, const char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-    int n;
-    n = ft_strlen(s1);
-    while (n > 0 && *s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-        n--;
-    }
-    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	int	n;
+
+	n = ft_strlen(s1);
+	while (n > 0 && *s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
-double ft_atof(const char *nptr)
+double	ft_atof(const char *str)
 {
-    double val = 0.0;
-    int sign = 1;
-    int decimal = 0;
-    while (*nptr == ' ')
-        nptr++;
-    if (*nptr == '-')
-    {
-        sign = -1;
-        nptr++;
-    }
-    while (*nptr >= '0' && *nptr <= '9')
-    {
-        val = val * 10.0 + (*nptr++ - '0');
-        if (decimal)
-            decimal++;
-    }
-    if (*nptr == '.')
-    {
-        nptr++;
-        while (*nptr >= '0' && *nptr <= '9')
-        {
-            val = val + ((*nptr++ - '0') / pow(10, decimal));
-            decimal++;
-        }
-    }
-    return sign * val;
+	double	intd;
+	double	decd;
+	double	sign;
+	int		i;
+
+	intd = 0.0;
+	decd = 0.0;
+	sign = 1.0;
+	if (*str == '+' || *str == '-')
+		if (*str++ == '-')
+			sign = -1.0;
+	while (ft_isdigit(*str))
+		intd = intd * 10 + (*str++ - '0');
+	i = -1;
+	if (*str == '.' && *str++)
+	{
+		while (ft_isdigit(*str))
+			decd += (pow(10, i--) * (*str++ - '0'));
+	}
+	return (sign * (intd + decd));
 }
 
-
-void ft_clean(t_data *data)
+void	ft_clean(t_data *data)
 {
 	mlx_destroy_image(data->mlx, data->img);
-    mlx_destroy_window(data->mlx, data->win);
-    mlx_destroy_display(data->mlx);
-    free(data->mlx);
-    exit(0);
+	mlx_destroy_window(data->mlx, data->win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	exit(0);
 }
